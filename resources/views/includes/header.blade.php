6df384d28993bf5bb6cf06bdf84948f27ee0855f
mouheb-ria <mouhebabderrahim@gmail.com>
2022-07-26 14:20:30 +0100

    HEADER COMMIT

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -109,6 +109,15 @@
                                 </svg>
                                 <span class="ml-2">Logout </span>
                             </a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                {{ __('Déconnecter') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
                         </div>
                     </li>
                 </ul>
